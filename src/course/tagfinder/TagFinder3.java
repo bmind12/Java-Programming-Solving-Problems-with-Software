466283ee2b10c72f42150fff752e5fc1135aca06
Sergey <ryabovs@yahoo.com>
2020-07-01 06:34:39 +0200

    implements lastPart method

--- src/course/tagfinder/TagFinder3.java
+++ src/course/tagfinder/TagFinder3.java
@@ -16,9 +16,20 @@ public class TagFinder3 {
         return secondOccurrence != -1;
     }
 
+    private static String lastPart(String stringA, String stringB) {
+        int stringALength = stringA.length();
+        int lastPartStartsAt = stringB.indexOf(stringA);
+
+        if (lastPartStartsAt == -1) return stringB;
+
+        return stringB.substring(lastPartStartsAt + stringALength);
+    }
+
     private static void testing() {
         System.out.println(twoOccurrences("a", "banana"));
         System.out.println(twoOccurrences("by", "A story by Abby Long"));
         System.out.println(twoOccurrences("atg", "ctgtatgta"));
+        System.out.println(lastPart("an", "banana")); //  "ana"
+        System.out.println(lastPart("zoo", "forest")); // "forest"
     }
 }
