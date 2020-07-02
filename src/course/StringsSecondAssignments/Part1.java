df0f7a48106f3404a2ab5f8becfcefa495df846e
Sergey <ryabovs@yahoo.com>
2020-07-02 07:14:43 +0200

    implements Testing method

--- src/course/StringsSecondAssignments/Part1.java
+++ src/course/StringsSecondAssignments/Part1.java
@@ -2,6 +2,7 @@ package course.StringsSecondAssignments;
 
 public class Part1 {
     public static void main(String[] args) {
+        testFindStopCodon();
     }
 
     private static int findStopCodon(String dna, int startIndex, String stopCodon) {
@@ -14,5 +15,18 @@ public class Part1 {
         }
 
         return dna.length();
+    }
+
+    private static void testFindStopCodon() {
+        int test1 = findStopCodon("ATGTAA", 3, "TAA");
+        System.out.println(test1);
+        int test2 = findStopCodon("ATGTTAATAA", 0, "TAA");
+        System.out.println(test2);
+        int test3 = findStopCodon("ATGTAATAA", 2, "TAA");
+        System.out.println(test3);
+        int test4 = findStopCodon("ATGTAATAA", 5, "TAA");
+        System.out.println(test4);
+        int test5 = findStopCodon("ATGTAATAA", 7, "TAA");
+        System.out.println(test5);
     }
 }
