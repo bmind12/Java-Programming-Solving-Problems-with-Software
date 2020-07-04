677d3af225aa33f45938531b2310f49fb586a0d1
Sergey <ryabovs@yahoo.com>
2020-07-04 04:17:15 +0200

    implements cgRatio method

--- src/course/StringsThirdAssignments/Part21.java
+++ src/course/StringsThirdAssignments/Part21.java
@@ -4,7 +4,7 @@ import edu.duke.*;
 
 public class Part21 {
     public static void main(String[] args) {
-        testGetAllGenes();
+        testCGRatio();
     }
 
     private static int findStopCodon(String dna, int startIndex, String stopCodon) {
@@ -91,4 +91,25 @@ public class Part21 {
         StorageResource data = getAllGenes("ATGTGAAAATTTAAATGTAGATGTTTTAATAGTAATAGATGTTTTGATAATAGTAGATGTTTTTTTTTAAATGTGA");
         System.out.println(data.data());
     }
+
+    private static void testCGRatio() {
+        float test1 = cgRatio("ATGCCATAG");
+        System.out.println(test1); // .4444444
+    }
+
+    private static float cgRatio(String dna) {
+        float cgCount = 0;
+
+        for (char c : dna.toCharArray()) {
+            if (c == 'C' || c == 'G') cgCount++;
+        }
+
+        return cgCount / dna.length();
+    }
+
+//
+//    Hint: 9/2 uses integer division because you are dividing an integer by an integer and thus Java thinks you want the result to be an integer. If you want the result to be a decimal number, then make sure you convert one of the integers to a decimal number by changing it to a float. For example, (float) 9/2 is interpreted by Java as 9.0/2 and if one of the numbers is a decimal, then Java assumes you want the result to be a decimal number. Thus (float) 9/2 is 4.5.
+//
+//    Write a method countCTG that has one String parameter dna, and returns the number of times the codon CTG appears in dna.
+//
 }
