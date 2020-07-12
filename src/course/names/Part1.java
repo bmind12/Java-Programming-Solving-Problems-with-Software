2818794579589e1aeb9afe5435a5b83a95f7c19f
Sergey <ryabovs@yahoo.com>
2020-07-12 04:47:09 +0200

    implements getRank method

--- src/course/names/Part1.java
+++ src/course/names/Part1.java
@@ -8,7 +8,10 @@ public class Part1 {
     public static void main(String[] args) {
         FileResource fr = new FileResource("./assets/04-01-names/example-small.csv");
 
-        totalBirths(fr);
+        int rank1 = getRank(2012,"Mason", "M"); // 2
+        int rank2 = getRank(2012,"Mason", "F"); // -1
+        System.out.println(rank1);
+        System.out.println(rank2);
     }
 
     private static void totalBirths(FileResource fr) {
@@ -30,5 +33,25 @@ public class Part1 {
         System.out.println("Girl names in the file: " + girlNamesCount);
         System.out.println("Boy names in the file: " + boysNamesCount);
         System.out.println("Total names in the file: " + (girlNamesCount + boysNamesCount));
+    }
+
+    private static int getRank(int year, String name, String gender) {
+        FileResource fr = new FileResource("./assets/04-01-names/yob" + year + "short.csv");
+        CSVParser parser = fr.getCSVParser();
+        int rank = 1;
+
+        for (CSVRecord record : parser) {
+            String nameRecord = record.get(0);
+            String genderRecord = record.get(1);
+
+            if (genderRecord.equals(gender)) {
+                if (nameRecord.equals(name)) return rank;
+
+                rank++;
+            };
+
+        }
+
+        return -1;
     }
 }
